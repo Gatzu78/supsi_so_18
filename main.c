d91528c22f18c27e778953a3cbb8a9f9492abf1f
Antonio Daniele Mu <antoniodaniele.mu@student.supsi.ch>
2018-03-16 20:46:41 +0100

    Add first code exemple

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 //
-// Created by attilio on 02/03/18.
+// Created by Attilio, Antonio, Luciano & Stefano
 //
-#include <stdio.h>
 
-int main() {
+execution_context thread1_ctx;
+execution_context thread2_ctx;
 
-    printf("All the bases are belong to us");
+void thread1() {
+    for (int i = 0; i < 10000; i++) {
+        save_context(thread1_ctx);
+        restore_context(thread2_ctx);
+    }
+}
 
-};
+void thread2() {
+    for(int i=0; i<10000; i++) {
+        // Do some computation
+        save_context(thread2_ctx);
+        restore_context(thread1_ctx);
+    }
+}
+
+void main() {
+    while(1) {
+        thread1();
+        thread2();
+    }
+}
