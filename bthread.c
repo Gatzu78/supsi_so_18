4290c55cf3589b1591edb220881c540b5ce42133
Antonio Daniele Mu <antoniodaniele.mu@student.supsi.ch>
2018-05-11 14:30:49 +0200

    aggiunto bthread_join

--- bthread.c
+++ bthread.c
@@ -3,6 +3,9 @@
 //
 
 #include "bthread.h"
+#define CUSHION_SIZE 10000
+#define save_context(CONTEXT) setjmp(CONTEXT)
+#define restore_context(CONTEXT) longjmp(CONTEXT, 1)
 
 //var di supporto
 static __bthread_scheduler_private * ourScheduler;
@@ -24,8 +27,20 @@ int bthread_create(bthread_t *bthread, const bthread_attr_t *attr, void *(*start
      ///
 }
 
-int bthread_join(bthread_t bthread, void **retval){
-
+int bthread_join(bthread_t bthread, void **retval) {
+    volatile __bthread_scheduler_private* scheduler = bthread_get_scheduler();
+    if (save_context(scheduler->context) == 0) {
+        bthread_initialize_next();
+        restore_context(scheduler->context);
+    } else {
+        __bthread_private* tp;
+        do {
+            if (bthread_reap_if_zombie(bthread, retval)) return 0;
+            scheduler->current_item = tqueue_at_offset(scheduler->current_item, 1);
+            tp = (__bthread_private*) tqueue_get_data(scheduler->current_item);
+        } while (tp->state != __BTHREAD_READY);
+        restore_context(tp->context);
+    }
 }
 
 void bthread_yield(){
